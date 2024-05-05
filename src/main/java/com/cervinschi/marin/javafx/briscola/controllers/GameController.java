6cc504f4cd24c1f3508852dd0d198e5b550af5bc
MarinCervinschi <marincervinschi3@gmail.com>
2024-05-05 22:39:32 +0200

    ci(GameController): refactor game controller

--- src/main/java/com/cervinschi/marin/javafx/briscola/controllers/GameController.java
+++ src/main/java/com/cervinschi/marin/javafx/briscola/controllers/GameController.java
@@ -1,14 +1,32 @@
 package com.cervinschi.marin.javafx.briscola.controllers;
-
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Alert;
+
 
 public class GameController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void newGame() {
+        System.out.println("Game started");
     }
+
+    @FXML
+    protected void showRules() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Rules");
+        alert.setHeaderText("Briscola rules");
+        alert.setContentText("The game is played with a deck of 40 cards. " +
+                "The game is played by two players, or four players in fixed partnerships. " +
+                "Players face each other, and the game is played clockwise. The player who " +
+                "did not deal the cards starts the game. The player who wins the trick leads " +
+                "the next trick. The game is won by the first player to reach 61 points. " +
+                "If both players reach 61 points in the same hand, the game is a tie.");
+        alert.showAndWait();
+    }
+    @FXML
+    protected void exit() {
+        System.exit(0);
+    }
+
 }
+
