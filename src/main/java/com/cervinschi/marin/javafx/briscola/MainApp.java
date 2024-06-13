39281c10b230890c77520c35beb6f1b2191b042d
MarinCervinschi <marincervinschi3@gmail.com>
2024-06-13 17:10:40 +0200

    feat(style): added style file

--- src/main/java/com/cervinschi/marin/javafx/briscola/MainApp.java
+++ src/main/java/com/cervinschi/marin/javafx/briscola/MainApp.java
@@ -1,12 +1,14 @@
 package com.cervinschi.marin.javafx.briscola;
 
-import static com.cervinschi.marin.javafx.briscola.utils.Const.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+
+import static com.cervinschi.marin.javafx.briscola.utils.Const.HEIGHT;
+import static com.cervinschi.marin.javafx.briscola.utils.Const.WIDTH;
 
 public class MainApp extends Application {
     @Override
